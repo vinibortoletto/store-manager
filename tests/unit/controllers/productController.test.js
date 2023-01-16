22b0db4f0c6efb7017f6680569968f659c96f404
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 17:53:46 -0300

    test: update mocks and http status code for product controller

--- tests/unit/controllers/productController.test.js
+++ tests/unit/controllers/productController.test.js
@@ -5,25 +5,30 @@ const sinonChai = require("sinon-chai");
 const { expect } = chai;
 chai.use(sinonChai);
 
-const mock = require("./mocks/productController.mock");
+const { productMock } = require("../mocks");
 const { productController } = require("../../../src/controllers");
 const { productService } = require("../../../src/services");
+const httpStatus = require("../../../src/utils/httpStatus");
 
 describe("Unit tests for productController", function () {
+  afterEach(function () { sinon.restore() })
+  
   it("should get all products", async function () {
     const res = {};
     const req = {};
-    const output = mock.productsResponse
+    
+    const output = productMock.getAllWithSuccess
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
+    
     sinon
       .stub(productService, "getAll")
       .resolves(output);
 
     await productController.getAll(req, res);
 
-    expect(res.status).to.have.been.calledWith(200);
-    expect(res.json).to.have.been.calledWith(mock.productsResponse);
+    expect(res.status).to.have.been.calledWith(httpStatus.OK);
+    expect(res.json).to.have.been.calledWith(output);
   });
 });
