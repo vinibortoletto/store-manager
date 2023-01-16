e02142cc47bed96ccf9c35100fe8e74ec0d23802
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 17:03:33 -0300

    test: import mocks and controller/services functions

--- tests/unit/controllers/productController.test.js
+++ tests/unit/controllers/productController.test.js
@@ -1,23 +1,29 @@
 const chai = require("chai");
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
-const mocks = require("./mocks/productController.mock");
 
 const { expect } = chai;
 chai.use(sinonChai);
+
+const mock = require("./mocks/productController.mock");
+const { productController } = require("../../../src/controllers");
+const { productService } = require("../../../src/services");
 
 describe("Unit tests for productController", function () {
   it("should get all products", async function () {
     const res = {};
     const req = {};
+    const output = mock.productsResponse
 
     res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns()
-    sinon.stub(productService, 'getAll').resolves(productsResponse)
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productService, "getAll")
+      .resolves(output);
 
-    await productController.getAll(req, res)
+    await productController.getAll(req, res);
 
-    expect(res.status).to.have.been.calledWith(200)
-    expect(res.json).to.have.been.calledWith(mocks.productsResponse)
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(mock.productsResponse);
   });
 });
