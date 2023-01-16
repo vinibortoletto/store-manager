5d06274a5c3084ae3184db65c6e5c5f265ca9d22
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 19:57:03 -0300

    feat: model for finding product by id

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,11 +1,17 @@
 const connection = require('../../connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute(
-    'SELECT * FROM products',
-  );
-
+  const [result] = await connection.execute('SELECT * FROM products');
   return result;
 };
 
-module.exports = { getAll };
+const findById = async (id) => {
+  const product = await connection.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [id],
+  );
+
+  return product;
+};
+
+module.exports = { getAll, findById };
