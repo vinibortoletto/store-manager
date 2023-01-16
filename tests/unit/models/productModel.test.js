c1235abc24aa4f5a15d68158b8d963e7d6a663d5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 19:56:53 -0300

    test: model for finding product by id

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -1,14 +1,25 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
-const { productModel } = require('../../../src/models');
-const connection = require('../../../connection');
-const { productMock } = require('../mocks');
+const { expect } = require("chai");
+const sinon = require("sinon");
+const { productModel } = require("../../../src/models");
+const connection = require("../../../connection");
+const { productMock } = require("../mocks");
 
-describe('Unit tests for productModel', function() {
-  it('should get all products with success', async function() {
-    const output = productMock.getAllWithSuccess
-    sinon.stub(connection, 'execute').resolves([output])
-    const result = await productModel.getAll()
-    expect(result).to.deep.equal(output)
+describe("Unit tests for productModel", function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it("should get all products with success", async function () {
+    const output = productMock.getAllWithSuccess;
+    sinon.stub(connection, "execute").resolves([output]);
+    const result = await productModel.getAll();
+    expect(result).to.deep.equal(output);
+  });
+
+  it("should find product by id with success", async function () {
+    const output = productMock.getAllWithSuccess[0];
+    sinon.stub(connection, "execute").resolves(output);
+    const result = await productModel.findById(1);
+    expect(result).to.deep.equal(output);
   });
 });
