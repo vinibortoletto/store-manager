ba27714849484327bbe9863ebcffbdedc6180a0d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-17 15:50:38 -0300

    feat: POST request for route products

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.findById);
+router.post('/', productController.insert);
 
 module.exports = router;
