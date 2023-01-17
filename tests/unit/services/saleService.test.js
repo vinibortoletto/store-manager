832fde38974921f24cbd7977bf9500c6e2e049fe
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-17 20:05:14 -0300

    test: service for inserting new sale with success

---
+++ tests/unit/services/saleService.test.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const { saleModel } = require("../../../src/models");
+const { saleService } = require("../../../src/services");
+const { saleMock } = require("../mocks");
+
+describe("Unit tests for saleService", function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it("should insert new sale with success", async function () {
+    sinon
+      .stub(saleModel, "insert")
+      .resolves(saleMock.insertResponseWithSuccess.id);
+
+    sinon
+      .stub(saleModel, "findById")
+      .resolves(saleMock.insertResponseWithSuccess);
+
+    const result = await saleService.insert(saleMock.insertBodyWithSuccess);
+
+    expect(result.type).to.equal(null);
+    expect(result.message).to.equal(saleMock.insertResponseWithSuccess);
+  });
+});
