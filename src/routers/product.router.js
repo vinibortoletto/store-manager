1ba6c53b9fb1841649e676014c2e06b7c26d776f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 23:27:03 -0300

    feat: router for finding product by id

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -4,5 +4,6 @@ const { productController } = require('../controllers');
 const router = express.Router();
 
 router.get('/', productController.getAll);
+router.get('/:id', productController.findById);
 
 module.exports = router;
