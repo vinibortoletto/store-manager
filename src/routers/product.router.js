2bf11f2110eb2950f7bed950bb66dd89aff004bc
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-17 18:28:02 -0300

    feat: add middleware to POST request

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productController } = require('../controllers');
+const { validateName } = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.findById);
-router.post('/', productController.insert);
+router.post('/', validateName, productController.insert);
 
 module.exports = router;
