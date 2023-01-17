0de87ce6c94df8325682508fb523dc0a7450474a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 23:17:12 -0300

    feat: controller fail case for finding product by id

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 const { productService } = require('../services');
+const { errorTypes } = require('../utils/errorTypes');
 const { OK } = require('../utils/httpStatus');
 
 const getAll = async (req, res) => {
@@ -8,8 +9,10 @@ const getAll = async (req, res) => {
 
 const findById = async (req, res) => {
   const { id } = req.params;
-  const product = await productService.findById(id);
-  res.status(OK).json(product);
+  const { type, message } = await productService.findById(id);
+
+  if (type) return res.status(errorTypes[type]).json({ message });
+  res.status(OK).json({ message });
 };
 
 module.exports = { getAll, findById };
