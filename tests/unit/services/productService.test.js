fb03ab1665fcccd50936f856966f7e35b9e978e0
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 23:14:56 -0300

    test: product services

--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -1,29 +1,47 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const { expect } = require("chai");
+const sinon = require("sinon");
 
-const { productModel } = require('../../../src/models');
-const { productService } = require('../../../src/services');
-const { productMock } = require('../mocks');
+const { productModel } = require("../../../src/models");
+const { productService } = require("../../../src/services");
+const { productMock } = require("../mocks");
 
-describe('Unit tests for productService', function() {
-  afterEach(function() {sinon.restore()})
-
-  it('should get all products', async function() {
-    sinon.stub(productModel, 'getAll').resolves(productMock.getAllWithSuccess)
-    const result = await productService.getAll()
-    expect(result).to.deep.equal(productMock.getAllWithSuccess)
+describe("Unit tests for productService", function () {
+  afterEach(function () {
+    sinon.restore();
   });
 
-  it('should find product by id with success', async function () {
-    const output = productMock.getAllWithSuccess[0]
-    sinon.stub(productModel, 'findById').resolves(output)
-    const result = await productService.findById(1)
-    expect(result).to.deep.equal(output)
-  })
+  it("should get all products", async function () {
+    sinon.stub(productModel, "getAll").resolves(productMock.getAllWithSuccess);
+    const result = await productService.getAll();
 
-  it('should fail to find product by id', async function () {
-    sinon.stub(productModel, 'findById').resolves([[]])
-    const result = await productService.findById(0)
-    expect(result).to.equal('Product not found')
-  })
+    expect(result).to.deep.equal(productMock.getAllWithSuccess);
+  });
+
+  it("should find product by id with success", async function () {
+    const output = productMock.getAllWithSuccess[0];
+    sinon.stub(productModel, "findById").resolves(output);
+
+    const result = await productService.findById(1);
+
+    expect(result.type).to.equal(null);
+    expect(result.message).to.deep.equal(output);
+  });
+
+  it("should fail to find product with invalid id", async function () {
+    sinon.stub(productModel, "findById").resolves([[]]);
+    const result = await productService.findById(0);
+
+    expect(result.message).to.equal('"id" must be a number greater than 1');
+  });
+
+  it("should fail to find product with non existent id", async function () {
+    sinon.stub(productModel, "findById").resolves(undefined);
+    const output = { type: "PRODUCT_NOT_FOUND", message: "Product not found" };
+    const nonExistentId = 999;
+
+    const result = await productService.findById(nonExistentId);
+
+    expect(result.type).to.equal(output.type);
+    expect(result.message).to.equal(output.message);
+  });
 });
