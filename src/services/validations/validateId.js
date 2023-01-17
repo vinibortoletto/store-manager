775128cd56b21dbd8c3a4d11f3df2440112bb3a6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-16 23:14:31 -0300

    feat: id validation

--- src/services/validations/validateId.js
+++ src/services/validations/validateId.js
@@ -2,7 +2,13 @@ const { idSchema } = require('./schema');
 
 const validateId = (id) => {
   const { error } = idSchema.validate(id);
-  if (error) return 'Product not found';
+  
+  if (error) {
+    return {
+      type: 'INVALID_VALUE', 
+      message: '"id" must be a number greater than 1',
+    };
+  }
 };
 
 module.exports = { validateId };
