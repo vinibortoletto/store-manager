cd5eb0d5857c0b47d32131a3f004913bab599999
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-18 18:41:31 -0300

    test: service for inserting new sale with success

--- tests/unit/services/saleProductService.test.js
+++ tests/unit/services/saleProductService.test.js
@@ -1,26 +1,22 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
+const { saleProductModel } = require("../../../src/models");
 
-// const { saleModel } = require("../../../src/models");
-// const { saleService } = require("../../../src/services");
-// const { saleMock } = require("../mocks");
+const { saleProductService } = require("../../../src/services");
+const { saleMock } = require("../mocks");
 
-describe("Unit tests for saleProductService", function () {
+describe.only("Unit tests for saleProductService", function () {
   afterEach(function () {
     sinon.restore();
   });
 
   it("should insert new sale with success", async function () {
     sinon
-      .stub(saleModel, "insert")
+      .stub(saleProductModel, "insert")
       .resolves(saleMock.insertResponseWithSuccess.id);
 
-    sinon
-      .stub(saleModel, "findById")
-      .resolves(saleMock.insertResponseWithSuccess);
-
-    const result = await saleService.insert(saleMock.insertBodyWithSuccess);
-
+    const result = await saleProductService.insert(saleMock.insertBodyWithSuccess);
+    
     expect(result.type).to.equal(null);
     expect(result.message).to.equal(saleMock.insertResponseWithSuccess);
   });
