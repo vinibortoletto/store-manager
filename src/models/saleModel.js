5572d93577aa3b2eb39cfc619a82a8e6bbfab962
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-18 17:44:05 -0300

    fix: insert query

--- src/models/saleModel.js
+++ src/models/saleModel.js
@@ -2,7 +2,7 @@ const connection = require('../../connection');
 
 const insert = async () => {
   const [{ insertId }] = await connection.execute(
-    'INSERT INTO sales (date) VALUES (NOW())',
+    'INSERT INTO sales () VALUES ()',
 );
 
   return insertId;
