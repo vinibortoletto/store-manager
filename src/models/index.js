f2d25e38f9e1a63c8f759d021ac35a9b05d592a6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-18 22:18:35 -0300

    change: remove saleProduct model and service

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,7 @@
 const productModel = require('./productModel');
 const saleModel = require('./saleModel');
-const saleProductModel = require('./saleProductModel');
 
 module.exports = {
   productModel,
   saleModel,
-  saleProductModel,
 };
