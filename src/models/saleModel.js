c68976e1db804742698be4d70fa672b7e9d78560
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-18 21:58:52 -0300

    feat: service for inserting new sale with success

--- src/models/saleModel.js
+++ src/models/saleModel.js
@@ -1,11 +1,28 @@
 const connection = require('../../connection');
+const { createColumnsAndPlaceholders } = require('../utils/createColumnsAndPlaceholders');
 
-const insert = async () => {
+const insertSale = async () => {
   const [{ insertId }] = await connection.execute(
     'INSERT INTO sales () VALUES ()',
-);
-
+  );
+  
   return insertId;
+};
+
+const insert = async (productList) => {
+  const newSaleId = await insertSale();
+
+  const { columns, placeholders } = createColumnsAndPlaceholders(productList[0]);
+
+  const query = `INSERT INTO sales_products (sale_id, ${columns}) VALUES (?, ${placeholders})`;
+
+  const promises = productList.map(async () => (
+    connection.execute(query, [newSaleId, ...Object.keys(productList[0])])
+  ));
+
+  await Promise.all(promises);
+
+  return newSaleId;
 };
 
 module.exports = { insert };
