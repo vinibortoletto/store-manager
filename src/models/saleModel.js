ad6e8ee62747162f337611ede50f826279d835eb
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-19 17:42:05 -0300

    fix: model for inserting new sale

--- src/models/saleModel.js
+++ src/models/saleModel.js
@@ -1,8 +1,6 @@
-const camelize = require('camelize');
 const connection = require('../../connection');
-const { createColumnsAndPlaceholders } = require('../utils/createColumnsAndPlaceholders');
 
-const insertSale = async () => {
+const insert = async () => {
   const [{ insertId }] = await connection.execute(
     'INSERT INTO sales () VALUES ()',
   );
@@ -10,26 +8,4 @@ const insertSale = async () => {
   return insertId;
 };
 
-const findById = async (id) => {
-  const query = 'SELECT * FROM sales_products WHERE id = ?';
-  const result = await connection.execute(query, [id]);
-  return camelize(result);
-};
-
-const insert = async (productList) => {
-  const { columns, placeholders } = createColumnsAndPlaceholders(productList[0]);
-
-  const newSaleId = await insertSale();
-  const query = `INSERT INTO sales_products (sale_id, ${columns}) VALUES (?, ${placeholders})`;
-
-  const promises = productList.map(async () => (
-    connection.execute(query, [newSaleId, ...Object.keys(productList[0])])
-  ));
-
-  await Promise.all(promises);
-
-  const insertedProductList = await findById(newSaleId);
-  return insertedProductList;
-};
-
-module.exports = { insert, findById };
+module.exports = { insert };
--- tests/unit/models/saleModel.test.js
+++ tests/unit/models/saleModel.test.js
@@ -11,24 +11,9 @@ describe("Unit tests for saleModel", function () {
   });
 
   it("should insert new sale with success", async function () {
-    const output = saleMock.insertResponseWithSuccess.id;
-
-    sinon.stub(connection, 'execute')
-      .onFirstCall()
-      .resolves([{insertId: output}])
-      .onSecondCall()
-      .resolves([{insertId: output}])
-    
+    const output = saleMock.insertResponseWithSuccess.id
+    sinon.stub(connection, 'execute').resolves([{ insertId: output }])
     const result = await saleModel.insert(saleMock.insertBodyWithSuccess)
-
-    expect(result).to.equal(output)
-  });
-
-  it('should find sale by id with success', async function () {
-    const output = saleMock.insertResponseWithSuccess
-    sinon.stub(connection, 'execute').resolves(output)
-    const result = await saleModel.findById(output.id)
-
     expect(result).to.deep.equal(output)
-  })
+  });
 });
