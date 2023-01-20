36b6256b21088aa08ff0f1b10cd6933733ce2cdd
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-20 20:29:26 -0300

    feat: find by id mock

--- tests/unit/mocks/sale.mock.js
+++ tests/unit/mocks/sale.mock.js
@@ -20,8 +20,16 @@ const insertBodyWithoutQuantity = [{ productId: 1 }];
 const insertBodyWithWrongQuantity = [{ productId: 1, quantity: 0 }];
 
 const findByIdResponseWithSuccess = [
-  { saleId: 3, productId: 1, quantity: 1 },
-  { saleId: 3, productId: 2, quantity: 5 },
+  {
+    date: "2021-09-09T04:54:29.000Z",
+    productId: 1,
+    quantity: 2,
+  },
+  {
+    date: "2021-09-09T04:54:54.000Z",
+    productId: 2,
+    quantity: 2,
+  },
 ];
 
 const getAllResponseWithSuccess = [
