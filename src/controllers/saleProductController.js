68cf43c28ba02f01663633ec7eeb624950a4b976
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-20 15:25:56 -0300

    fix: output values

--- src/controllers/saleProductController.js
+++ src/controllers/saleProductController.js
@@ -5,7 +5,7 @@ const httpStatus = require('../utils/httpStatus');
 const insert = async (req, res) => {
   const { type, message } = await saleProductService.insert(req.body);
   if (type) return res.status(errorTypes[type]).json({ message });
-  res.status(httpStatus.CREATED).json({ message });
+  res.status(httpStatus.CREATED).json(message);
 };
 
 module.exports = { insert };
