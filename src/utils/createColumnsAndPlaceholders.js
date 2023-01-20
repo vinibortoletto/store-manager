202fcc5128ba62fd08988d76e55195ae86c102b8
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-19 21:27:58 -0300

    feat: snakeize object

--- src/utils/createColumnsAndPlaceholders.js
+++ src/utils/createColumnsAndPlaceholders.js
@@ -1,5 +1,7 @@
+const snakeize = require('snakeize');
+
 const createColumnsAndPlaceholders = (object) => {
-  const columns = Object.keys(object).join(', ');
+  const columns = Object.keys(snakeize(object)).join(', ');
   const placeholders = Object.keys(object).map(() => '?').join(', ');
   return { columns, placeholders };
 };
