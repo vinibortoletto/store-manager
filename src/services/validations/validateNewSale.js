45e65633fd5c38556d262de1a1c32e3a2c065b5e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-20 13:12:33 -0300

    feat: fail case for inserting sale with quantity less or equal 0

--- src/services/validations/validateNewSale.js
+++ src/services/validations/validateNewSale.js
@@ -2,10 +2,11 @@ const { newSaleSchema } = require('./schema');
 
 const validateNewSale = (newSale) => {
   const { error } = newSaleSchema.validate(newSale);
+  const hasQuantity = newSale[0].quantity;
 
   if (error) {
     return {
-      type: 'VALUE_REQUIRED',
+      type: !hasQuantity ? 'INVALID_VALUE' : 'REQUIRED_VALUE',
       message: error.message.split('[0].').join(''),
     };
   }
