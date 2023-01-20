f2ca4b9874ee23d152677c06561a9c435c00cb0f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-19 21:28:24 -0300

    fix: insert sale

--- src/services/saleProductService.js
+++ src/services/saleProductService.js
@@ -2,13 +2,22 @@ const { saleProductModel } = require('../models');
 
 const insert = async (productList) => {
   const newSaleId = await saleProductModel.insert(productList);
-  const newSale = await saleProductModel.findById(newSaleId);
+  const newSaleProductList = await saleProductModel.findById(newSaleId);
+
+  const newSale = {
+    id: newSaleId,
+    itemsSold: newSaleProductList.map(({ productId, quantity }) => ({
+      productId,
+      quantity,
+    })),
+  };
+
   return { type: null, message: newSale };
 };
 
 const findById = async (id) => {
   const sale = await saleProductModel.findById(id);
-  return { type: null, message: sale };  
+  return { type: null, message: sale };
 };
 
 module.exports = { insert, findById };
