8a02c98182fd4f129aff0a948f505eafe0e088af
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-19 22:35:26 -0300

    feat: fail case for inserting new sale without productId

--- src/controllers/saleProductController.js
+++ src/controllers/saleProductController.js
@@ -1,9 +1,11 @@
 const { saleProductService } = require('../services');
+const { errorTypes } = require('../utils/errorTypes');
 const httpStatus = require('../utils/httpStatus');
 
 const insert = async (req, res) => {
   const { type, message } = await saleProductService.insert(req.body);
-  res.status(httpStatus.CREATED).json(message);
+  if (type) return res.status(errorTypes[type]).json({ message });
+  res.status(httpStatus.CREATED).json({ message });
 };
 
 module.exports = { insert };
