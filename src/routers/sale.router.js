811256af6f758c20258416fa9aec298cd153a068
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-20 20:30:28 -0300

    feat: get sales routes

--- src/routers/sale.router.js
+++ src/routers/sale.router.js
@@ -4,5 +4,7 @@ const { saleProductController } = require('../controllers');
 const router = express.Router();
 
 router.post('/', saleProductController.insert);
+router.get('/', saleProductController.getAll);
+router.get('/:id', saleProductController.findById);
 
 module.exports = router;
