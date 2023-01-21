5646ac667d43f703772e1c96ebf34da96aa5fd08
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-21 15:00:00 -0300

    feat: delete product route

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -7,5 +7,6 @@ router.get('/', productController.getAll);
 router.get('/:id', productController.findById);
 router.post('/', productController.insert);
 router.put('/:id', productController.update);
+router.delete('/:id', productController.delete);
 
 module.exports = router;
