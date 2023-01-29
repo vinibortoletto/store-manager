60b9c2a87fb2d4cee775a491095b66f7e26b4022
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-29 17:41:58 -0300

    feat: remove sale route

--- src/routers/sale.router.js
+++ src/routers/sale.router.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/', saleProductController.insert);
 router.get('/', saleProductController.getAll);
 router.get('/:id', saleProductController.findById);
+router.delete('/:id', saleProductController.remove);
 
 module.exports = router;
