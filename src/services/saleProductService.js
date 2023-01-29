20a47a6b0220c541c08dc2a75510934e92c76237
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-29 18:56:55 -0300

    feat: update sale with success service

--- src/services/saleProductService.js
+++ src/services/saleProductService.js
@@ -6,7 +6,7 @@ const { validateProductList } = require('./validations/validateProductList');
 const insert = async (productList) => {
   const newSaleError = validateNewSale(productList);
   if (newSaleError) return newSaleError;
- 
+
   const productListError = await validateProductList(productList);
   if (productListError) return productListError;
 
@@ -23,15 +23,15 @@ const insert = async (productList) => {
 const findById = async (id) => {
   const error = validateId(id);
   if (error) return error;
-  
+
   const sale = await saleProductModel.findById(id);
 
-  if (sale.length === 0) return { type: 'SALE_NOT_FOUND', message: 'Sale not found' };
+  if (sale.length === 0) { return { type: 'SALE_NOT_FOUND', message: 'Sale not found' }; }
   return { type: null, message: sale };
 };
 
 const getAll = async () => {
-  const saleList = await saleProductModel.getAll();  
+  const saleList = await saleProductModel.getAll();
   return { type: null, message: saleList };
 };
 
@@ -43,9 +43,21 @@ const remove = async (id) => {
   return { type: null, message: '' };
 };
 
+const update = async (updatedSale, saleId) => {
+  await saleProductModel.update(updatedSale, saleId);
+
+  const result = {
+    saleId,
+    itemsUpdated: updatedSale,
+  };
+  
+  return { type: null, message: result };
+};
+
 module.exports = {
   insert,
   findById,
   getAll,
   remove,
+  update,
 };
