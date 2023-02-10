ea55599d050477c1c96badedb2360e83535eb077
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-10 11:13:10 -0300

    feat: search product route

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -3,11 +3,11 @@ const { productController } = require('../controllers');
 
 const router = express.Router();
 
+router.get('/search', productController.search);
 router.get('/', productController.getAll);
 router.get('/:id', productController.findById);
 router.post('/', productController.insert);
 router.put('/:id', productController.update);
 router.delete('/:id', productController.remove);
-router.get('/search', productController.search);
 
 module.exports = router;
